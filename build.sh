f366ee80b13cef992a75b020a67c3ca21b04613d
jwd2a <jdavis1002@gmail.com>
2018-09-05 16:45:34 -0400

    Added favicons

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 rm -rf dist
 mkdir dist
 cp src/*.html dist
+cp src/*.png dist
+cp src/*.ico dist
+cp src/*.svg dist
+cp src/*.xml dist
+cp src/site.webmanifest dist
 cp -R src/images dist
 
 sass src/scss/main.scss dist/main.css
