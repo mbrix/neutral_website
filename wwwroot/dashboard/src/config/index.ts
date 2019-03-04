9bda34fa9ddef26d9cee57b9bdc322450369a7ad
calebsevern <csevern@neutralproject.com>
2019-03-04 15:41:49 -0500

    feat(dashboard): pull collateral line info

---
+++ wwwroot/dashboard/src/config/index.ts
@@ -0,0 +1,35 @@
+export default {
+  vaultAddress: '0x28B291E74bcE603004B52921Ec9Ad3DDb6F85e44',
+  nusd: {
+    symbol: 'NUSD',
+    name: 'Neutral Dollar',
+    address: '0x0237443359aB0b11EcDC41A7aF1C90226a88c70f',
+    decimals: 12,
+  },
+  tokens: [
+    {
+      symbol: 'TUSD',
+      name: 'True USD',
+      address: '0xAE4C9bD0f7AE5398Df05043079596E2BF0079CE9',
+      decimals: 18,
+    },
+    {
+      symbol: 'USDC',
+      name: 'USD Coin',
+      address: '0x403cc7802725928652a3d116Bb1781005e2e76d3',
+      decimals: 6,
+    },
+    {
+      symbol: 'DAI',
+      name: 'Dai',
+      address: '0x162A36c9821eadeCFF9669A3940b7f72d055Cd1c',
+      decimals: 18,
+    },
+    {
+      symbol: 'PAX',
+      name: 'Paxos Standard Token',
+      address: '0x40023EAc4d2fCf91ce93B68C789aE14Aaa120d0E',
+      decimals: 18,
+    },
+  ],
+};
